7bfbb487bcf411d6991c3f755ee7662f2d9c0ded
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-09-18 20:32:06 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -92,3 +92,14 @@ async def upgrade(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["hotstar"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "hotstar")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HOT_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )    
