9eaf5c7308cb1bf8a6e0640c6167d1e99e45dbe4
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-09-18 19:59:11 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -66,7 +66,6 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        prase_mod="html", 
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
